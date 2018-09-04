ed73d7ef6bd436b150a4b2b688abf4ef73cab22e
Rene <rene.souza@gmail.com>
2018-09-04 18:39:09 -0300

    [billing-react] - Adicionando paginação

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -12,24 +12,40 @@ class Pagination extends Component{
             this.setState({
                 ...this.state,
                 pages: this.props.pages.map((_, idx)=>{
-                    return {idx , selected: false}
+                    return {id:idx , selected: false}
                 })
             });
         }
     }
 
-    setSelected(idx){
+    setSelected(id){
+        let deselecAll = {
+            ...this.state
+        }
 
+        deselecAll.pages.map(page=>{
+            if(page.id !== id){
+                page.selected = false;
+            }
+            else{
+                page.selected = true;
+            }
+
+        });
+
+       this.setState({
+           ...deselecAll
+       })
     }
 
     render(){
         let pagination = null;
 
         if(this.state.pages){
-            pagination = this.state.pages.map((_, idx)=>{
+            pagination = this.state.pages.map(page=>{
                 return (
-                <li key={`pg-${idx}`}>
-                    <Button click={()=>this.setSelected(idx).bind(this)}>{idx + 1}</Button>
+                <li key={`pg-${page.id}`}>
+                    <Button selected={page.selected} click={()=>this.setSelected(page.id)}>{page.id + 1}</Button>
                 </li>);
             });
         }
